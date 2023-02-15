ee4ea0c04e96398fa5a266fd13afb14729389a05
0xbm <0xmb@protonmail.com>
2023-02-15 18:23:03 +0100

    commit

--- main.py
+++ main.py
@@ -4,51 +4,58 @@ import string
 print("Password Generator")
 
 
-def __init__(self):
-    chars = string.ascii_letters + string.digits + string.punctuation
-    number = int(input("Amount of password to generate: "))
-    length = int(input("Your password length: "))
-
-
 def create_file():
     f = open("passwd.txt", "w")
-    f.write("Created new file")
+    print("Created file: passwd.txt")
     f.close()
+    main()
 
 
 def append_to_file():
     f = open("passwd.txt", "a")
-    f.write("Now the file has more content!")
+    print("Now the file has more content!")
+    f.write("asdsadas")
     f.close()
+    main()
 
 
 def gen_passwd():
-    # chars = string.ascii_letters + string.digits + string.punctuation
-    # number = int(input("Amount of password to generate: "))
-    # length = int(input("Your password length: "))
+    chars = string.ascii_letters + string.digits + string.punctuation
+    number = int(input("Amount of password to generate: "))
+    length = int(input("Your password length: "))
     print("\n\x1b[5;30;41m" + "Here are your password(s):" + "\x1b[0m")
-
+    pswd = []
     for i in range(number):
         passwords = ""
         for j in range(length):
             passwords += random.choice(chars)
-            print(i, "passwd", passwords)
+        print(i + 1, "password ->", passwords)
+        pswd.append(passwords)
+    print(pswd)
+
+    choice = input("Append to file? y/n")
+    match choice:
+        case "y":
+            f = open("passwd.txt", "a")
+            print("Now the file has more content!")
+            f.write("pswd.append(passwords())")
+            f.close()
+        case "n":
+            main()
 
 
-def main(self):
+def main():
     choice = input(
-        "What would you like to do?\n1.Create file to pass,\n2.Append pass to file, \n3.Gen passwd or q for quit: "
+        "What would you like to do?\n1.Create file to pass,\n2.Gen passwd or q for quit: "
     )
     match choice:
         case "1":
             create_file()
         case "2":
-            append_to_file()
-        case "3":
             gen_passwd()
         case "q":
-            breakpoint
             print("\x1b[5;30;42m" + "QUIT" + "\x1b[0m")
+            quit()
 
 
-main(main)
+main()
