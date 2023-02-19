1139d07f0a4e7e8c071b2dec394bd1b226f874e2
0xbm <0xmb@protonmail.com>
2023-02-19 19:32:41 +0100

    some imporvment...in progress

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import random
 import string
+import glob
+import os.path
 
-print("Password Generator")
+print("-------------------")
+print("Password Generator|")
+print("-------------------")
 
 
 def create_file():
     f = open("passwd.txt", "w")
     print("Created file: passwd.txt")
     f.close()
-    main()
+    # f = open("passwd.txt", "a")
+    # print("Now the file has more content!!!!!!!!")
+    # print()
+    # f.write(
+    #     "pswd.append(passwords())------ dodany tekst po utworzeniu pliku else: create_file "
+    # )
+    # f.close()
+    # main()
 
 
 def append_to_file():
@@ -19,39 +30,49 @@ def append_to_file():
     main()
 
 
-def gen_passwd():
+pswd = []
+
+
+def gen_passwd(pswd):
     chars = string.ascii_letters + string.digits + string.punctuation
     number = int(input("Amount of password to generate: "))
     length = int(input("Your password length: "))
     print("\n\x1b[5;30;41m" + "Here are your password(s):" + "\x1b[0m")
-    pswd = []
+    # pswd = []
     for i in range(number):
         passwords = ""
         for j in range(length):
             passwords += random.choice(chars)
         print(i + 1, "password ->", passwords)
         pswd.append(passwords)
-    print(pswd)
+    print(pswd, "lista z dodanymi haslami")
 
-    choice = input("Append to file? y/n")
+    choice = input("Append to file? yes/no: ")
     match choice:
-        case "y":
-            f = open("passwd.txt", "a")
-            print("Now the file has more content!")
-            f.write("pswd.append(passwords())")
-            f.close()
-        case "n":
+        case "yes":
+            if os.path.isfile("passwd.txt") == True:
+                print("OK")
+                append_to_file()
+                # f = open("passwd.txt", "a")
+                # print("Now the file has more content!")
+                # f.write("pswd.append(passwords())")
+                # f.close()
+                main()
+            else:
+                print("nie ma ale utowrzse teraz")
+                create_file()
+                append_to_file()
+        case "no":
+            print("Back to main")
             main()
 
 
 def main():
     choice = input(
-        "What would you like to do?\n1.Create file to pass,\n2.Gen passwd or q for quit: "
+        "What would you like to do?\n1.Generate password(s) or q for quit:\n"
     )
     match choice:
         case "1":
-            create_file()
-        case "2":
             gen_passwd()
         case "q":
             print("\x1b[5;30;42m" + "QUIT" + "\x1b[0m")
