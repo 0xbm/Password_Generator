265b45dc81b20e92b561e628b329bedef47dbe0c
0xbm <0xmb@protonmail.com>
2023-02-20 18:49:38 +0100

    replace open file methods by 'with open'

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 import string
-import glob
 import os.path
 
 print("-------------------")
@@ -11,54 +10,39 @@ pswd = []
 
 
 def create_file():
-    f = open("passwd.txt", "w")
-    print("Created file: passwd.txt")
-    f.close()
-    # f = open("passwd.txt", "a")
-    # print("Now the file has more content!!!!!!!!")
-    # print()
-    # f.write(
-    #     "pswd.append(passwords())------ dodany tekst po utworzeniu pliku else: create_file "
-    # )
-    # f.close()
-    # main()
+    with open("passwd.txt", "w") as file:
+        # print("Created file: passwd.txt")
+        return file
 
 
 def append_to_file(pswd):
-    f = open("passwd.txt", "a")
-    print("Now the file has more content!")
-    s = "".join(pswd)
-    print(pswd)
-    f.write(s)
-    f.close()
+    with open("passwd.txt", "a") as file:
+        for ps in pswd:
+            file.write(str(ps) + "\n")
+    # f = open("passwd.txt", "a")
+    # s = "\n".join(str(x) for x in pswd)
+    # f.write(s)
+    # f.close()
     main()
 
 
 def gen_passwd(pswd):
-
     chars = string.ascii_letters + string.digits + string.punctuation
     number = int(input("Amount of password to generate: "))
     length = int(input("Your password length: "))
     print("\n\x1b[5;30;41m" + "Here are your password(s):" + "\x1b[0m")
-    pswd = []
     for i in range(number):
         passwords = ""
         for j in range(length):
             passwords += random.choice(chars)
         print(i + 1, "password ->", passwords)
         pswd.append(passwords)
-    print(pswd, "lista z dodanymi haslami")
 
     choice = input("Append to file? yes/no: ")
     match choice:
         case "yes":
             if os.path.isfile("passwd.txt") == True:
-                print("OK")
                 append_to_file(pswd)
-                # f = open("passwd.txt", "a")
-                # print("Now the file has more content!")
-                # f.write("pswd.append(passwords())")
-                # f.close()
                 main()
             else:
                 print("nie ma ale utowrzse teraz")
@@ -71,7 +55,7 @@ def gen_passwd(pswd):
 
 def main():
     choice = input(
-        "What would you like to do?\n1.Generate password(s) or q for quit:\n"
+        "What would you like to do?\n1.Generate password(s) or q for quit: \n"
     )
     match choice:
         case "1":
