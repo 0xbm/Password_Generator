bde84318fb72b1355ae97f9022d27c858c7eca8c
0xbm <0xmb@protonmail.com>
2023-02-22 19:06:39 +0100

    commit

---
+++ encrypt.py
@@ -0,0 +1,19 @@
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+
+with open("filekey.key", "wb") as filekey:
+    filekey.write(key)
+
+with open("filekey.key", "rb") as filekey:
+    key = filekey.read()
+
+fernet = Fernet(key)
+
+with open("passwd.csv", "rb") as file:
+    original = file.read()
+
+encrypted = fernet.encrypt(original)
+
+with open("passwd.csv", "wb") as encrypted_file:
+    encrypted_file.write(encrypted)
